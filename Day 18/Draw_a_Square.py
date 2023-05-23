09768e550dbff3419250079082609d5f8629b14a
Sang Nguyen <hoangsang203@gmail.com>
2023-05-23 17:35:56 +0700

    Practice!

---
+++ Day 18/Draw_a_Square.py
@@ -0,0 +1,7 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.speed(1000)
+for each in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
